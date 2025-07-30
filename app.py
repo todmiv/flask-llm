d54337dcbef15b8e6dbd8625c98d247ba9c6bb9d
labintsev <andrej.labintsev@yandex.ru>
2025-07-30 14:36:39 +0300

    refactor LLMService , move instance to model.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template, request, jsonify
-from models import LLMService, db, ChatHistory, dummy_llm_service
+from models import db, ChatHistory, dummy_llm_service, chat_with_llm
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db.init_app(app)
-app.config["LLM_SERVICE"] = LLMService("Ты оператор техподдержки, отвечай вежливо")
-
 
 @app.route("/")
 def home():
@@ -18,7 +16,8 @@ def chat():
     data = request.get_json()
     user_message = data.get("message", "")
 
-    llm_reply = app.config["LLM_SERVICE"].chat(user_message)
+    # llm_reply = dummy_llm_service(user_message)
+    llm_reply = chat_with_llm(user_message)
     # Save to chat history
     new_entry = ChatHistory(user_message=user_message, llm_reply=llm_reply)
     db.session.add(new_entry)
